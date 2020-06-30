fc88424f4e08bb9502a99dd6dcd5be2e38f8fc82
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2020-06-29 21:22:38 -0500

    Update OtherTools.py

--- API/OtherTools.py
+++ API/OtherTools.py
@@ -1,10 +1,10 @@
 import sys
 from API.ApiMain import CustomConsole
+
 # ApiMain [begin|options|help|create]
 # ApiMain --begin/-b -d/--d <directory> or -f <file> <json>
 # ApiMain --options/-o id
 # ApiMain --help/-h//? return this tooltip
-
 
 
 def update_readme():
@@ -12,3 +12,9 @@ def update_readme():
     s = rm.read()
     s = s.format(__docstring__=CustomConsole.help_string)
     rm.write(s)
+
+if __name__ == "__main__":
+    uarg = sys.argv[1:]
+    if len(uarg) > 0:
+        if "-ur" in uarg:
+            update_readme()
